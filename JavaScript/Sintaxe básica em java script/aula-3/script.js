22349d597d162f91bd69db10469d401e95b1fac7
Luan Marques <lm7007fernandes@gmail.com>
2021-12-30 17:56:09 -0300

    Calculadora usando somente JavaScript

---
+++ JavaScript/Sintaxe básica em java script/aula-3/script.js
@@ -0,0 +1,58 @@
+//  vetores
+
+// como declarar  um array 
+    /*let array = ['String' ,1, true];
+    console.log(array); */
+
+    //pode guardar vários tipos de dados
+    //  let array = ['string,', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+    //  console.log(array)
+
+// push
+// array.push('novo item');
+
+// //forEach
+// array.forEach(function(item, index){console.log(item, index)})
+
+// pop
+// array.pop();
+// console.log(array);
+
+//shift
+// array.shift();
+// console.log(array);
+
+//unshift
+// array.unshift('novo item no inicio');
+// console.log(array);
+
+//splice 
+// array.splice(0, 3);
+// console.log(array);
+
+
+// aula 4 
+
+// var jogador1 = -2;
+// var jogador2 = 0;
+// var placar;
+
+// if (jogador1 = -1) {
+//     console.log('Jogador inválido')
+//     if (jogador1 > 0) {
+//         console.log('Jogador 1 marcou ponto!');      
+// }   else if (jogador2 > 0) {
+//         console.log('jogador 2 marcou ponto!')
+// }   else {
+//         console.log('Ninguém marcou ponto!')
+// }
+
+// }
+    
+// calculadora 
+// function calculadora() {
+//     const operação = prompt ('Escolha sua operação:'); 
+    
+    
+// }
+
