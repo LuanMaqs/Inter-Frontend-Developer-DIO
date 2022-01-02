87e954680dc45eb6c06df87003556694079a9046
Luan Marques <lm7007fernandes@gmail.com>
2022-01-02 17:15:27 -0300

    minha netflix

---
+++ Inter-Frontend-Developer-DIO/Inter-Frontend-Developer-DIO/JavaScript/Sintaxe básica em java script/aula 1 e 2/script.js
@@ -0,0 +1,31 @@
+// Parte 1 = aprendizado
+
+// alert("Tudo certo jovem?")
+
+// boolean
+// var vOuF = false; 
+// console.log(typeof(vOuF));
+// console.log(vOuF);
+
+// // number
+// var numeroQualquer = 1;
+// console.log(numeroQualquer);
+// console.log(typeof(numeroQualquer));
+
+// // String
+// var nome = 'Luan';
+// console.log(nome);
+// console.log(typeof(nome));
+
+
+// como declarar
+// var varialvel = 'Luan';
+// varialvel = 'Marques';
+// console.log(varialvel);
+
+// let variavel2 = 'luan';
+// console.log(variavel2)
+
+
+const constante = 'Luan';
+console.log(constante)
